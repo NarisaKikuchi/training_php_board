ad3d340873859825ff4f6c5e10b9c8a0a1f8ea16
Narisa Kikuchi <narisakikuchi@NarisanoMacBook-Air.local>
2022-05-17 15:18:08 +0900

    タスク５の削除

--- docker/web/php/createAccount.php
+++ docker/web/php/createAccount.php
@@ -6,18 +6,18 @@
 
 <body>
     <div class="body">
-        <div class="header_left">
-            <header class="header_letter">
+        <div class="header-left">
+            <header class="header-letter">
                 Bulletine Board
             </header>
         </div>
 
-        <div class="login_screen upper">
+        <div class="login-screen upper">
             <h1>Bulletine Board</h1>
-            <p class="login_letter">新規追加画面</p>
+            <p class="login-letter">新規追加画面</p>
         </div>
 
-        <div class="login_screen greybox">
+        <div class="login-screen greybox">
             <div>
                 <h2>新規追加</h2>
                 <p>ユーザーIDとパスワードを登録してください。</p>
@@ -25,14 +25,14 @@
 
             <div>
                 <input type="text" value="" placeholder="ユーザーID">
-                <div id="password_bottom">
+                <div id="password-margin">
                     <input type="text" value="" placeholder="パスワード">
                     <input type="text" value="" placeholder="パスワード確認">
                 </div>
             </div>
 
             <div>
-                <div class="login_button">
+                <div class="login-button">
                     <form type="submit">
                         <button onclick="location.href='login'">登録する</button>
                     </form>
